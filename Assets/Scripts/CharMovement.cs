552d38fbc1d2cdc17f61bb5a6c964a696f804c8e
Formuoli <41936538+Formuoli@users.noreply.github.com>
2018-08-03 18:27:04 -0500

    Basic Character Movement
    -New Scripts and Sprites Folder
    -New 265.walking testing scene
    -New CharMovement.cs Script for moving character

---
+++ Assets/Scripts/CharMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Move Character
+        Move();
+	}
+    
+    void Move()
+    {
+       
+            float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+            float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+            transform.Translate(new Vector3(x, y, 0));
+        
+    }
+}
